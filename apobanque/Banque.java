9a47263f5aaff2c48a93cce9446859ff4b6ca454
Djegomde <hippolyte.djegomde@gmail.com>
2023-09-23 17:06:17 +0200

    Creation d'un projet de gestion de compte Bancaire

---
+++ apobanque/Banque.java
@@ -0,0 +1,32 @@
+package apobanque;
+
+/**
+ * Le sujet pour compléter ces fichiers se trouve ici:
+ * http://l3miagelw.free.fr/apogrenoble/tp03/compte.html
+ */
+public class Banque {
+
+    // Nom de la banque
+    String nom;
+
+    // Liste des comptes clients
+    CompteBancaire[] comptes;
+
+    Banque(String nom, CompteBancaire[] comptes) {
+    // TODO
+    }
+
+    String description() {
+        return null;
+    // TODO
+    }
+
+    double avoirTotal() {
+        return 0;
+    // TODO
+    }
+
+    void afficheNumeroComptesDebitsPlanchers() {
+    // TODO
+    }
+}
---
+++ apobanque/CompteBancaire.java
@@ -0,0 +1,8 @@
+package apobanque;
+
+/**
+ * Le sujet pour compléter ces fichiers se trouve ici:
+ * http://l3miagelw.free.fr/apogrenoble/tp03/compte.html
+ */
+class CompteBancaire {
+}
