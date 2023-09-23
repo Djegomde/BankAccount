fe525325ba83c187513b181a37d9b1d79ff3f67c
Djegomde <hippolyte.djegomde@gmail.com>
2023-09-23 23:59:11 +0200

    commit

--- apobanque/CompteBancaire.java
+++ apobanque/CompteBancaire.java
@@ -5,4 +5,92 @@ package apobanque;
  * http://l3miagelw.free.fr/apogrenoble/tp03/compte.html
  */
 class CompteBancaire {
+
+    // Declaration des attributs de la classe CompteBancaire
+    int numero;
+    String client;
+    double solde;
+    double decouvert;
+
+    // Premier Constructeur
+    CompteBancaire() {
+        this.numero = 0;
+        this.client = "John Doe";
+        this.solde = 0.0;
+        this.decouvert = 0.0;
+    }
+
+    // Deuxieme Constructeur
+    CompteBancaire(int numero, String nom) {
+        this.numero = numero;
+        this.client = nom;
+        this.solde = 100;
+        this.decouvert = 50;
+    }
+
+    // Troisième Constructeur
+    CompteBancaire(int numero, String nom, double somme) {
+        this.numero = numero;
+        this.client = nom;
+        this.solde = (somme / 2);
+        this.decouvert = (somme / 2);
+    }
+
+    // méthode qui permet de deposer l'argent dans le compte
+    void depot(double montant) {
+        if (montant > 0) {
+            this.solde = solde + montant;
+        } else {
+            System.out.println("Montant ne peut pas être négatif");
+            this.solde = solde + 0;
+        }
+
+    }
+
+    // méthode retrait qui effectue le retrait maximum possible et renvoie true si
+    // le client a pu obtenir la somme qu'il souhaitait, false sinon.
+    boolean retrait(double montant) {
+        boolean possible;
+        double maxi_autorise = (solde + decouvert);
+
+        if (montant >= 0 && montant < solde) {
+            solde -= montant;
+            possible = true;
+
+        } else if (montant >= 0 && montant <= maxi_autorise) {
+            solde = (maxi_autorise - montant);
+            possible = true;
+        } else {
+            possible = false;
+        }
+        return possible;
+    }
+
+    // méthodes estADecouvert() qui renvoie true si le solde du compte du client est
+    // négative.
+    boolean estADecouvert() {
+        if (this.solde > 0) {
+            return false;
+        }
+        return true;
+    }
+
+    int getNumero() {
+        return this.numero;
+    }
+
+    String getClient() {
+        return this.client;
+    }
+
+    double getSomme() {
+        return this.solde;
+
+    }
+
+    String infos() {
+        String text = "Client :" + this.client + "\n" + "Numéro  :" + this.numero + "\n" + "Solde :" + this.solde + "\n"
+                + "Decouverte :  " + this.decouvert + "\n";
+        return text;
+    }
 }
--- apobanque/TestCompteBancaire.java
+++ apobanque/TestCompteBancaire.java
@@ -6,8 +6,8 @@ package apobanque;
  */
 public class TestCompteBancaire {
 
-    public static void main(String[] args) {
-    /*
+	public static void main(String[] args) {
+
 		CompteBancaire inconnu = new CompteBancaire();
 		CompteBancaire ken = new CompteBancaire(2329, "Ken Bowie");
 		CompteBancaire alain = new CompteBancaire(2330, "Alain Die", 500);
@@ -22,31 +22,37 @@ public class TestCompteBancaire {
 
 		System.out.println("\nRetrait de " + somme + " euros...\n");
 		succes = inconnu.retrait(somme);
+		System.out.println(succes);
 		System.out.print(inconnu.infos());
 		if (!succes) {
-			System.out.println(inconnu.getClient() + " n'a pas pu retirer la somme de " + somme + "euros.\n");
+			System.out.println(inconnu.getClient() + " n'a pas pu retirer la somme de " +
+					somme + "euros.\n");
 		}
 
 		succes = ken.retrait(somme);
 		System.out.print(ken.infos());
 		if (!succes) {
-			System.out.println(ken.getClient() + " n'a pas pu retirer la somme de " + somme + "euros.\n");
+			System.out.println(ken.getClient() + " n'a pas pu retirer la somme de " +
+					somme + "euros.\n");
 		}
 
 		succes = alain.retrait(somme);
 		System.out.print(alain.infos());
 		if (!succes) {
-			System.out.println(alain.getClient() + " n'a pas pu retirer la somme de " + somme + "euros.\n");
+			System.out.println(alain.getClient() + " n'a pas pu retirer la somme de " +
+					somme + "euros.\n");
 		}
 
-		somme = 100.0;
-		System.out.println("\nDépôt de " + somme + " euros...\n");
-		inconnu.depot(somme);
-		System.out.print(inconnu.infos());
-		ken.depot(somme);
-		System.out.print(ken.infos());
-		alain.depot(somme);
-		System.out.print(alain.infos());
-		*/
-    }
+		/*
+		 * somme = 100.0;
+		 * System.out.println("\nDépôt de " + somme + " euros...\n");
+		 * inconnu.depot(somme);
+		 * System.out.print(inconnu.infos());
+		 * ken.depot(somme);
+		 * System.out.print(ken.infos());
+		 * alain.depot(somme);
+		 * System.out.print(alain.infos());
+		 */
+
+	}
 }
